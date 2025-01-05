712c2e3175ac6172699d4cc94c03e8609ffbe1ac
JosefineNorden <iorbanderas@gmail.com>
2025-01-05 13:27:24 +0100

    Interaction moved to userInterface

--- Bookshelf.cs
+++ Bookshelf.cs
@@ -13,75 +13,33 @@ namespace Digital_bookshelf
         public void AddBook(Book book)
         {
             _books.Add(book);
-
         }
 
-        public void AddCustomBook()
-        {
-            try
-            {
-                Console.WriteLine("Enter the title of your book:");
-                string title = Console.ReadLine();
-                Console.WriteLine("Enter the author of your book:");
-                string author = Console.ReadLine();
-                Console.WriteLine("Enter the genre of your book:");
-                string genre = Console.ReadLine();
-                Console.WriteLine("Enter the ISBN-number of your book:");
-                int isbn = int.Parse(Console.ReadLine());
-
-                AddBook(new Book(title, author, genre, isbn));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Something went wrong while adding the book {ex.Message}");
-            }
-
-            Console.WriteLine("Your book has been added successfully!");
-            Console.WriteLine("Press enter to return to the menu...");
-            Console.ReadLine();
-            Thread.Sleep(1000);
-            Console.WriteLine("...Loading...");
-            Thread.Sleep(1000);
-            Console.Clear();
-
-        }
-
-        public void RemoveBookByTitle()
+        public bool RemoveBookByTitle(string title)
 
         {
-            try
+            Book bookToRemove = _books.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (bookToRemove != null)
             {
-                Console.WriteLine("Enter the name of the book you wish to remove:");
-                string title = Console.ReadLine();
-
-                Book bookToRemove = _books.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
-                if (bookToRemove != null)
-                {
-                    _books.Remove(bookToRemove);
-                    Console.WriteLine($"{bookToRemove.Title} has been removed from the digital bookshelf.");
-                }
-                else
-                {
-                    Console.WriteLine($"The book {bookToRemove.Title} was not found.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Something went wrong while removing your book: {ex.Message}");
+                _books.Remove(bookToRemove);
+                return true;
             }
 
-            Console.WriteLine("Press enter to return to the menu...");
-            Console.ReadLine();
-            Thread.Sleep(1000);
-            Console.WriteLine("...Loading...");
-            Thread.Sleep(1000);
-            Console.Clear();
+            return false;
         }
         public void DisplayBooks()
         {
             if (_books.Count == 0)
             {
-                Console.WriteLine("You have no books in your digital bookshelf.");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("***   ***   ***");
+                Console.WriteLine("");
+                Console.ResetColor();
+                Console.WriteLine("- Empty-");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("");
+                Console.WriteLine("***   ***   ***");
+                Console.ResetColor();
             }
             else
             {
@@ -96,14 +54,6 @@ namespace Digital_bookshelf
                     Console.ResetColor();
                 }
             }
-
-            Console.WriteLine("Press enter to return to the menu...");
-            Console.ReadLine();
-            Thread.Sleep(1000);
-            Console.WriteLine("...Loading...");
-            Thread.Sleep(1000);
-            Console.Clear();
-
         }
     }
 }
